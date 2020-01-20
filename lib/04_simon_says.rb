1c209d4ce60070da8efc733e3457ac4e8e99df3f
Cheltonne <chelton1506@gmail.com>
2020-01-20 23:56:07 +0100

    last commit

---
+++ lib/04_simon_says.rb
@@ -0,0 +1,15 @@
+def echo(str)
+    return str
+end
+
+def shout(str)
+    return str.upcase
+end
+
+def repeat(str, n=2)
+    return n.times.map {str}.join(' ')
+end
+
+def start_of_word(str, i)
+    return str.slice(i-1)
+end
